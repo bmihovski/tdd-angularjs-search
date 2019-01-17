8de0d0dfec0587ddbf6fae0b028bc4d63ffd0873
Gogs <gogs@fake.local>
2019-01-17 17:01:43 +0200

    Squashed commit of the following:
    commit 630d0ffaa6f9147e300cd07174ae8d8c652d9e3b
    Author: Gogs <gogs@fake.local>
    Date:   Thu Jan 17 17:01:18 2019 +0200
    
    	e2e finished unfortunately the functionality is not working

--- app/product.js
+++ app/product.js
@@ -1,1 +1,8 @@
 angular.module('product', []);
+
+/*angular.module('product',['ngMockE2E'])
+ 		.run(['$httpBackend',function($httpBackend) {
+ 				var testProduct = productDataBuilder().build();
+ 				var products = [testProduct];
+ 				$httpBackend.whenGET('/products').respond(products);
+ 		  	}]);*/
